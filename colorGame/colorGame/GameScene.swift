ca5359b160e366b064a982d3083592d6decfcb51
Gareth Fuller <gareth.fuller@clark.de>
2017-10-08 15:13:41 +0200

    Added move up and down action for player

--- colorGame/colorGame/GameScene.swift
+++ colorGame/colorGame/GameScene.swift
@@ -13,6 +13,47 @@ class GameScene: SKScene {
     var tracksArray: [SKSpriteNode]? = [SKSpriteNode]()
     var player: SKSpriteNode?
     
+    
+    // MARK: lifecycle
+    // like view did load in UIKit
+    override func didMove(to view: SKView) {
+        setUpTracks()
+        createPlayer()
+        tracksArray?.first?.color = UIColor.green
+    }
+    
+    // MARK: Touch control
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            // location of the touch in root node
+            let location = touch.location(in: self)
+            let node = self.nodes(at: location).first
+            
+            // check the name of what we touched
+            if node?.name == "right" {
+                print("move right")
+            } else if node?.name == "up" {
+                moveVertically(up: true)
+            } else if node?.name == "down" {
+                moveVertically(up: false)
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        player?.removeAllActions()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        player?.removeAllActions()
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    // MARK: Adding items to the scene functions
     func setUpTracks() {
         for i in 0 ... 8 {
             if let track = self.childNode(withName: "\(i)") as? SKSpriteNode {
@@ -31,15 +72,11 @@ class GameScene: SKScene {
         self.addChild(player!)
     }
     
-    // like view did load in UIKit
-    override func didMove(to view: SKView) {
-        setUpTracks()
-        createPlayer()
-        tracksArray?.first?.color = UIColor.green
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    // MARK: moving function
+    func moveVertically(up: Bool) {
+        let amount = (up) ? 3 : -3 as CGFloat
+        let moveAction = SKAction.moveBy(x: 0, y: amount, duration: 0.01)
+        let repeatAction = SKAction.repeatForever(moveAction)
+        player?.run(repeatAction)
     }
 }
